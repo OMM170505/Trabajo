c526640663c4bbfb572ab315a4a4218abddfc8c9
unknown <olivermm1705@gmail.com>
2026-03-09 12:41:16 +0100

    Prueba

---
+++ prueba2.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hola, este es un miniprograma de prueba.")
+    nombre = input("¿Cómo te llamas? ")
+    print(f"¡Encantado de conocerte, {nombre}!")
+
+if __name__ == "__main__":
+    main()
+    
